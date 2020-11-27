401956725302c7b47678771f63c1373fa18ceb77
csr87 <74970184+csr87@users.noreply.github.com>
2020-11-27 07:21:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ conn.on('user-presence-update', json => console.log(`[ ${moment().format("HH:mm:
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by Root@MrG3P5`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by @sarangga87`)
 })
 
 conn.on('message-new', async(m) =>
